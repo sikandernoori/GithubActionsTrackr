b51ca4bfb874463c46327318c1e8bfaa17f2d486
Skandar Munir Ahmed <sikandernoori@gmail.com>
2022-09-20 19:13:01 +0300

    locally tested and working

--- code_sign.py
+++ code_sign.py
@@ -6,13 +6,28 @@ project = XcodeProject.load('ios/Runner.xcodeproj/project.pbxproj')
 
 team_id = sys.argv[1] # first argument for team_id
 issuer_id = sys.argv[2] # second argument for issuer_id
-profile_name = 'github_actions'
+profile_name = sys.argv[3]
 
 # Add code sign
 project.add_code_sign('iPhone Distribution',
 						  team_id,
 						  issuer_id,
 						  profile_name)
+project.remove_flags('CODE_SIGN_STYLE', 'Automatic')
+project.add_flags('CODE_SIGN_STYLE', 'manual')
+# save the project, otherwise your changes won't be picked up by Xcode
+project.save()
 
+
+# Signign Pods
+project = XcodeProject.load('ios/Pods/Pods.xcodeproj/project.pbxproj')
+
+# Add code sign
+project.add_code_sign('iPhone Distribution',
+                          team_id,
+                          issuer_id,
+                          "")
+
+project.add_flags("DEVELOPMENT_TEAM[sdk=iphoneos*]", team_id)
 # save the project, otherwise your changes won't be picked up by Xcode
 project.save()
